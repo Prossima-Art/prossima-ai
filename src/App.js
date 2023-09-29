c29e725faa01a5c913009128a03195ff0268a977
alanny-design <alannylucena@gmail.com>
2023-09-29 18:56:28 -0300

    feat: landing page for a prossima ai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import Img1 from "./components/assets/img-1.png";
+import Img2 from "./components/assets/img-2.png";
+import Img3 from "./components/assets/img-3.png";
+import Lottie from "lottie-react";
+import AnimationData from "./components/assets/Animation.json";
+import {Logo} from "./components/logo.jsx";
 
 function App() {
+  const observer = new IntersectionObserver(entries => {
+    for (let i = entries.length - 1; i >= 0; i--) {
+      const entry = entries[i]
+      if (entry.isIntersecting) {
+        document.querySelectorAll("[data-img]").forEach(img => {
+          img.classList.remove("show")
+        })
+        const img = document.querySelector(entry.target.dataset.imgToShow)
+        img?.classList.add("show")
+        break
+      }
+    }
+  })  
+  document.querySelectorAll("[data-img-to-show]").forEach(section => {
+    observer.observe(section)
+  })
+  useEffect(() => {
+  document.querySelectorAll("[data-img-to-show]").forEach(section => {
+    observer.observe(section)
+  })
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <header id="header">
+      <div className="hero">
+      <Logo 
+          className="logo"
+          alt="Prossima Art AI logo"
+          />
+      </div>
+      <button>Contact Us</button>
+
+    </header>
+
+      <div class="imgs">
+        <img
+          src={Img1}
+          alt="..."
+          data-img
+          id="img-1"
+          className="top-section-img show"
+        />
+        <img src={Img2} alt="..." data-img id="img-2" />
+        <img src={Img3} alt="..." data-img id="img-3" />
+      </div>
+      <section className="top-section full-screen-section">
+        <div className="left">
+          <h1>Innovations for B2B</h1>
+          <p>
+            The only platform that gives continuous service API for interactions across platforms.
+          </p>
+        </div>
+        <div className="right">
+          <Lottie className="top-section-animation"
+          animationData={AnimationData}
+          loop={true}
+          autoplay={true}
+          />
+        </div>
+      </section>
+      <section className="full-screen-section first-main-section">
+        <h1>Completely Visual</h1>
+        <p>Never touch the command line, from provision to production.</p>
+        <div data-img-to-show="#img-1"></div>
+      </section>
+      <section className="full-screen-section">
+        <h1>Full Stack</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Never manage infrastructure again. One click gets you: a database,
+          APIs, deployments, hosting, etc.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div data-img-to-show="#img-2"></div>
+      </section>
+      <section className="full-screen-section">
+        <h1>Sentiment analysis</h1>
+        <p>Organizations can leverage this analysis to gain insights into the perceptions and sentiments surrounding their brand or product.</p>
+        <div data-img-to-show="#img-3"></div>
+      </section>
+    </>
   );
 }
 
---
+++ src/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+
+export default function setScrollVar() {
+  const htmlElement = document.documentElement
+  const percentOfScreenHeightScrolled =
+    htmlElement.scrollTop / htmlElement.clientHeight
+  console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
+  htmlElement.style.setProperty(
+    "--scroll",
+    Math.min(percentOfScreenHeightScrolled * 100, 100)
+  )
+}
+
+setScrollVar()
+
+
