df24b71aca99bfd8d3e938347a5916cfcaa02473
horberlan <horberlan.brito@flowef.com>
2023-10-08 16:59:44 -0300

    chore: create logic to validate fields in the form and prints success message

--- src/components/offcanvas/Offcanvas.jsx
+++ src/components/offcanvas/Offcanvas.jsx
@@ -4,7 +4,7 @@ import { Button, Offcanvas, Form } from "react-bootstrap";
 import axios from "axios";
 import { Logo } from "../logo";
 
-export function ModalContato() {
+const ModalContato = () => {
   return (
     <>
       {["end"].map((placement, idx) => (
@@ -12,43 +12,65 @@ export function ModalContato() {
       ))}
     </>
   );
-}
+};
 
-export function OffCanvasExample({ ...props }) {
+const OffCanvasExample = ({ ...props }) => {
   const [show, setShow] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [isEmailForm, setIsEmailForm] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    setIsFormSubmitted(false);
+  };
 
-  const [Contacts, setContacts] = useState([]);
-  const [Name, setName] = useState("");
-  const [Email, setEmail] = useState("");
+  const handleShow = () => {
+    setShow(true);
+  };
 
   const getContacts = async () => {
-    const { data } = await axios.get(`https://prossima-be.vercel.app/contacts`)
-    .then((response) => response.data)
-    .catch((error) => console.log(error));
+    try {
+      const { data } = await axios.get(
+        `https://prossima-be.vercel.app/contacts`
+      );
+      setContacts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    setContacts(data);
+  const checkFields = (value, isEmail = false) => {
+    setShowErrorMessage(!value);
+
+    if (isEmail) {
+      const re = /\S+@\S+\.\S+/;
+      setIsEmailForm(re.test(value));
+    }
   };
 
   const addNewContacts = async () => {
-    if (!Name || !Email) {
-      return alert("Preencha os campos !");
+    if (!name || !email) {
+      setShowErrorMessage(true);
+      return;
     }
-    const data = { Name, Email };
+    const data = { Name: name, Email: email };
 
-    const { data: newContacts} = await axios.post(
-      `https://prossima-be.vercel.app/contacts`,
-      data
-    )
-    .then((response) => response.data)
-    .catch((error) => console.log(error))
-    ;
-
-    setContacts([...Contacts, newContacts]);
-    setName("");
-    setEmail("");
+    try {
+      const { data: newContacts } = await axios.post(
+        `https://prossima-be.vercel.app/contacts`,
+        data
+      );
+      setContacts([...contacts, newContacts]);
+      setName("");
+      setEmail("");
+      setIsFormSubmitted(true);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -78,67 +100,87 @@ export function OffCanvasExample({ ...props }) {
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <h4>Interested in seeing more? We'd love to have you</h4>
-          <p class="text-secondary">
-            Reach out to us by filling out the contact form below. We look
-            forward to hearing from you!
-          </p>
+          {isFormSubmitted ? (
+            <div>
+              <h4>Form Submitted Successfully!</h4>
+              <p className="text-secondary">
+              Thank you for contacting us.
+              </p>
+            </div>
+          ) : (
+            <div>
+              <h4>Interested in seeing more? We'd love to have you</h4>
+              <p className="text-secondary">
+                Reach out to us by filling out the contact form below. We look
+                forward to hearing from you!
+              </p>
 
-          <Form className="mb-5" onSubmit={addNewContacts}>
-            <Form.Group className="row-md-2 mx-2">
-              <Form.Label for="name" column sm="4">
-                Your name
-              </Form.Label>
-              <Form.Control
-                className="rounded-5 mb-3"
-                as="input"
-                placeholder="your name"
-                type="text"
-                id="name"
-                name="name"
-                required=""
-                rows={2}
-                value={Name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="row-md-3 mx-2 ">
-              <Form.Label column sm="4" for="email">
-                Email address
-              </Form.Label>
-              <Form.Control
-                className="rounded-5  mb-4"
-                type="email"
-                placeholder="name@example.com"
-                id="email"
-                name="email"
-                required=""
-                value={Email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Button
-              className="rounded-5"
-              style={{
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-              }}
-              variant="primary"
-              type="submit"
-              // onClick={() => send2DB(Name, Email)}
-            >
-              Join The Waitlist!
-            </Button>
-          </Form>
+              <Form className="mb-5">
+                <Form.Group className="row-md-2 mx-2">
+                  <Form.Label htmlFor="name" column sm="4">
+                    Your name
+                  </Form.Label>
+                  <Form.Control
+                    className="rounded-5 mb-3"
+                    as="input"
+                    placeholder="your name"
+                    type="text"
+                    id="name"
+                    name="name"
+                    required=""
+                    rows={2}
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                      checkFields(e.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="row-md-3 mx-2 ">
+                  <Form.Label column sm="4" htmlFor="email">
+                    Email address
+                  </Form.Label>
+                  <Form.Control
+                    className="rounded-5  mb-4"
+                    type="email"
+                    placeholder="name@example.com"
+                    id="email"
+                    name="email"
+                    required=""
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      checkFields(e.target.value, true);
+                    }}
+                  />
+                </Form.Group>
+                {showErrorMessage && <p>Please fill in the blanks above and click to send.</p>}
+                <Button
+                  className="rounded-5"
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                  }}
+                  variant="primary"
+                  disabled={!isEmailForm}
+                  onClick={addNewContacts}
+                >
+                  Join The Waitlist!
+                </Button>
+              </Form>
+            </div>
+          )}
         </Offcanvas.Body>
-        <p class="text-secondary text-center mb-5">
+        <p className="text-secondary text-center mb-5">
           Prefer email?{" "}
-          <a href="mailto:prossima.art@gmail.com" class="text-primary">
+          <a href="mailto:prossima.art@gmail.com" className="text-primary">
             prossima.art@gmail.com{" "}
           </a>
         </p>
       </Offcanvas>
     </>
   );
-}
+};
+
+export { ModalContato, OffCanvasExample };
